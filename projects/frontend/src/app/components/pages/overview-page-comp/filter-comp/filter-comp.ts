1d2873a13ffb9b4ffaf0be138f6b199007eb6a15
mo <moritz.hipper@outlook.de>
2026-01-11 21:05:27 +0100

    wp

--- projects/frontend/src/app/components/pages/overview-page-comp/filter-comp/filter-comp.ts
+++ projects/frontend/src/app/components/pages/overview-page-comp/filter-comp/filter-comp.ts
@@ -1,7 +1,11 @@
 import { Component, input, model } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { CollectionUser } from '@shared/types'
-import { mapToStaggerVM, StaggerVM } from 'projects/frontend/src/app/utils/genaral-utils'
+import {
+  createAppearRange,
+  mapToStaggerVM,
+  StaggerVM
+} from 'projects/frontend/src/app/utils/genaral-utils'
 import { IconComp } from '../../../shared/icon-comp/icon-comp'
 
 @Component({
@@ -15,4 +19,6 @@ export class FilterComp {
   collections = input.required<StaggerVM<CollectionUser>, CollectionUser[]>({
     transform: mapToStaggerVM
   })
+
+  protected delays = createAppearRange(1, 4)
 }
