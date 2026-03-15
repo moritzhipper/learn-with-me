2fa53405a510f2738c620957a759e22a5d0cbfc5
mo <moritz.hipper@outlook.de>
2026-03-15 22:50:00 +0100

    wp

--- projects/frontend/src/app/components/pages/translate-page-comp/magic-translate/magic-translate.ts
+++ projects/frontend/src/app/components/pages/translate-page-comp/magic-translate/magic-translate.ts
@@ -1,6 +1,9 @@
 import { Component, computed, inject, signal } from '@angular/core'
+import { toSignal } from '@angular/core/rxjs-interop'
 import { FormsModule, NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms'
 import { LearnableBase } from '@shared/types'
+import { LearnablesStore } from 'projects/frontend/src/app/store/learnablesStore'
+import { LearnableCreationConfig } from 'projects/frontend/src/app/types_and_schemas/types'
 import { IconComp } from '../../../shared/icon-comp/icon-comp'
 import { RadioComp } from '../../../shared/radio-comp/radio-comp'
 import { LearnableComp } from '../../overview-page-comp/learnable-comp/learnable-comp'
@@ -13,6 +16,7 @@ import { LearnableComp } from '../../overview-page-comp/learnable-comp/learnable
 })
 export class MagicTranslate {
   private readonly _fb = inject(NonNullableFormBuilder)
+  private readonly ls = inject(LearnablesStore)
   protected readonly proposedCards = signal<LearnableBase[]>([])
 
   form = this._fb.group({
@@ -22,12 +26,33 @@ export class MagicTranslate {
 
   imagePreview = signal<string | null>(null)
 
-  formSignal = signal(this.form.valueChanges)
+  formSignal = toSignal(this.form.valueChanges)
 
-  protected createLearnablesConfig = computed(() => {
-    // map form to config here
+  protected createLearnablesConfig = computed<LearnableCreationConfig | null>(() => {
+    const language = this.ls.activeBank().language
+    if (!language) return null
 
-    return {}
+    const imagePreview = this.imagePreview()
+    if (imagePreview) {
+      return {
+        source: 'image',
+        image: imagePreview,
+        type: 'both',
+        language
+      }
+    }
+
+    const form = this.formSignal()
+    if (form?.text && form?.type) {
+      return {
+        source: 'text',
+        text: form.text,
+        type: form.type as LearnableCreationConfig['type'],
+        language
+      }
+    }
+
+    return null
   })
 
   // create two rxMetho
