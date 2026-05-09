283278e91d24c2c55fd839f3692a6a98c4df3396
mo <moritz.hipper@outlook.de>
2026-05-09 12:54:28 +0200

    wp

--- projects/frontend/src/app/components/pages/stats-page/stats-page.ts
+++ projects/frontend/src/app/components/pages/stats-page/stats-page.ts
@@ -7,12 +7,20 @@ import { IconComp } from '../../shared/icon-comp/icon-comp'
 import { PageHeaderComp } from '../../shared/page-header-comp/page-header-comp'
 import { PageIconComp } from '../../shared/page-icon-comp/page-icon-comp'
 
-type PracticeHistoryDay = Record<number, PracticeHistoryItemSummary[]>
+type PracticeHistoryDay = Record<number, PracticeHistoryDaySummary>
 
 type PracticeHistoryItemSummary = PracticeActive & {
+  collectionName?: string
   right: number
   wrong: number
   unanswered: number
+}
+
+type PracticeHistoryDaySummary = {
+  practices: PracticeHistoryItemSummary[]
+  totalRight: number
+  totalWrong: number
+  totalUnanswered: number
 }
 
 @Component({
@@ -25,22 +33,43 @@ type PracticeHistoryItemSummary = PracticeActive & {
 export class StatsPage {
   private readonly ls = inject(LearnablesStore)
 
-  protected readonly practiceHistory = computed<PracticeHistoryDay>(() =>
-    this.ls.activeBank().practice.history.reduce<PracticeHistoryDay>((acc, item) => {
+  protected readonly practiceHistory = computed<PracticeHistoryDay>(() => {
+    const collections = this.ls.activeBank().collections
+
+    return this.ls.activeBank().practice.history.reduce<PracticeHistoryDay>((acc, item) => {
       const dayOfPractice = convertToDayPrecisionUTCDate(item.createdAt)
+
+      const collectionName =
+        item.type === 'collection'
+          ? collections.find((c) => c.id === item.collectionId)?.name
+          : undefined
+
       const summary: PracticeHistoryItemSummary = {
         ...item,
+        collectionName,
         right: item.guessables.filter((r) => r.guess === 'right').length,
         wrong: item.guessables.filter((r) => r.guess === 'wrong').length,
         unanswered: item.guessables.filter((r) => r.guess === 'unanswered').length
       }
 
-      if (acc[dayOfPractice]) {
-        acc[dayOfPractice] = [...acc[dayOfPractice], summary]
+      const daySummary = acc[dayOfPractice]
+
+      if (daySummary) {
+        acc[dayOfPractice] = {
+          practices: [...daySummary.practices, summary],
+          totalRight: daySummary.totalRight + summary.right,
+          totalWrong: daySummary.totalWrong + summary.wrong,
+          totalUnanswered: daySummary.totalUnanswered + summary.unanswered
+        }
       } else {
-        acc[dayOfPractice] = [summary]
+        acc[dayOfPractice] = {
+          practices: [summary],
+          totalRight: summary.right,
+          totalWrong: summary.wrong,
+          totalUnanswered: summary.unanswered
+        }
       }
       return acc
     }, {})
-  )
+  })
 }
