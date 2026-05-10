433eeeb58f2699054153651acd15ce08db50c950
mo <moritz.hipper@outlook.de>
2026-05-10 11:22:59 +0200

    wp

--- projects/frontend/src/app/components/pages/dashboard-page/dashboard-page.ts
+++ projects/frontend/src/app/components/pages/dashboard-page/dashboard-page.ts
@@ -7,6 +7,7 @@ import { HeaderLink } from '../../shared/header-link/header-link'
 import { PageHeaderComp } from '../../shared/page-header-comp/page-header-comp'
 import { PageIconComp } from '../../shared/page-icon-comp/page-icon-comp'
 import { PracticeQuickActions } from '../../shared/practice-quick-actions/practice-quick-actions'
+import { PracticeTimeline } from '../../shared/practice-timeline/practice-timeline'
 import { SharedBankComp } from '../share-page-comp/shared-collection-comp/shared-bank-comp'
 
 @Component({
@@ -17,7 +18,8 @@ import { SharedBankComp } from '../share-page-comp/shared-collection-comp/shared
     HeaderLink,
     PracticeQuickActions,
     CardsQuickSelector,
-    SharedBankComp
+    SharedBankComp,
+    PracticeTimeline
   ],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.scss',
@@ -26,6 +28,7 @@ import { SharedBankComp } from '../share-page-comp/shared-collection-comp/shared
 export class DashboardPage {
   private readonly apiS = inject(ApiService)
   private readonly ls = inject(LearnablesStore)
+  protected readonly practiceHistory = computed(() => this.ls.activeBank().practice.history)
 
   sharedBanks = rxResource({
     params: computed(() => this.ls.activeBank().language),
