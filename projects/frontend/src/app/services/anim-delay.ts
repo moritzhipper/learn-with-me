c5ce09c6fc4f5edad8d734d16bedc9528966618b
mo <moritz.hipper@outlook.de>
2026-05-27 19:41:00 +0200

    cleaned up animation utils even more

--- projects/frontend/src/app/services/anim-delay.ts
+++ projects/frontend/src/app/services/anim-delay.ts
@@ -1,23 +1,24 @@
-import { computed, Directive, input } from '@angular/core'
+import { Directive, input } from '@angular/core'
 
 type AnimConfig = { i: number; duration?: number } & ({ size: number } | { list: unknown[] })
 
 @Directive({
   selector: '[animDelay]',
   host: {
-    '[style.animationDelay.s]': 'animationDelay()'
+    '[style.animationDelay.s]': 'animDelay()'
   }
 })
 export class AnimDelay {
-  readonly animDelay = input.required<AnimConfig>()
+  readonly animDelay = input.required<Number, AnimConfig>({
+    transform: (conf) => this.mapToAnimDelay(conf)
+  })
   private readonly DURATION_DEFAULT = 0.3
 
-  protected readonly animationDelay = computed(() => {
-    const conf = this.animDelay()
+  protected mapToAnimDelay(conf: AnimConfig): number {
     const size = 'size' in conf ? conf.size : conf.list.length
     const i = conf.i
     const duration = conf.duration ?? this.DURATION_DEFAULT
 
     return i * (duration / size)
-  })
+  }
 }
