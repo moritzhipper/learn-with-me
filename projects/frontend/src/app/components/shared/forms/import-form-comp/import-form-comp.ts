ba8cab5d1fcf4f862dbc3f942d4a552a447194e4
mo <moritz.hipper@outlook.de>
2026-07-18 22:31:15 +0200

    WP

--- projects/frontend/src/app/components/shared/forms/import-form-comp/import-form-comp.ts
+++ projects/frontend/src/app/components/shared/forms/import-form-comp/import-form-comp.ts
@@ -3,13 +3,12 @@ import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms'
 import { BankShareBase, LanguageConfig } from '@shared/types'
 import { AnimDelay } from 'projects/frontend/src/app/services/anim-delay'
 import { BankImportOptions } from 'projects/frontend/src/app/types/types'
-import { InfoCard } from '../../info-card/info-card'
 import { RadioComp } from '../../radio-comp/radio-comp'
 import { BaseModalDirective } from '../base-modal-directive'
 
 @Component({
   selector: 'app-import-form-comp',
-  imports: [ReactiveFormsModule, RadioComp, AnimDelay, InfoCard],
+  imports: [ReactiveFormsModule, RadioComp, AnimDelay],
   templateUrl: './import-form-comp.html',
   styleUrl: './import-form-comp.scss'
 })
@@ -17,29 +16,6 @@ export class ImportFormComp extends BaseModalDirective {
   private readonly _fb = inject(NonNullableFormBuilder)
   bank = input.required<BankShareBase>()
   activeBankLanguage = input.required<LanguageConfig>()
-
-  summary = computed(() => {
-    const b = this.bank()
-    const maxPreviewCards = 10
-    const maxPreviewCollections = 20
-
-    const cardsCount = b.learnables.length
-    const previewCards = b.learnables.slice(0, maxPreviewCards)
-    const cardsCountHidden = cardsCount - previewCards.length
-
-    const collectionsCount = b.collections.length
-    const previewCollections = b.collections.slice(0, maxPreviewCollections)
-    const collectionsCountHidden = collectionsCount - previewCollections.length
-
-    return {
-      cardsCount,
-      previewCards,
-      cardsCountHidden,
-      collectionsCount,
-      previewCollections,
-      collectionsCountHidden
-    }
-  })
 
   langMatch = computed(() => {
     const bankL = this.bank().language
