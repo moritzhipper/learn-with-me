2376dfbe75474053fde56a88af5eec389438da1c
mo <moritz.hipper@outlook.de>
2026-07-29 13:13:50 +0200

    wp

--- projects/frontend/src/app/components/pages/cards-page/cards-page.ts
+++ projects/frontend/src/app/components/pages/cards-page/cards-page.ts
@@ -1,49 +1,12 @@
-import { Component, computed, inject } from '@angular/core'
-import { RouterLink } from '@angular/router'
-import { Collection } from '@shared/types'
-import { LearnablesStore } from '../../../store/learnables-store'
-import { aggregateConfidence, ConfidenceAggregate } from '../../../utils/genaral-utils'
-import { UserCollection } from '../../shared/banks-and-collections/user-collection/user-collection'
-import { ConfidenceStats } from '../../shared/confidence/confidence-stats/confidence-stats'
+import { Component } from '@angular/core'
+import { CardsQuickSelector } from '../../shared/banks-and-collections/cards-quick-selector/cards-quick-selector'
 import { PageHeaderComp } from '../../shared/page-header-comp/page-header-comp'
 import { PageWrapper } from '../page-wrapper/page-wrapper'
 
-type CollectionWithConfidence = {
-  collection: Collection
-  confidence: ConfidenceAggregate
-}
-
 @Component({
   selector: 'liz-cards-page',
-  imports: [PageWrapper, PageHeaderComp, UserCollection, RouterLink, ConfidenceStats],
+  imports: [PageWrapper, PageHeaderComp, CardsQuickSelector],
   templateUrl: './cards-page.html',
   styleUrl: './cards-page.scss'
 })
-export class CardsPage {
-  private readonly ls = inject(LearnablesStore)
-
-  protected collectionsWithConfidence = computed<CollectionWithConfidence[]>(() => {
-    const collections = this.ls.collections()
-    const allCards = this.ls.learnables()
-
-    return collections.map((collection) => {
-      const cardsInCollection = allCards.filter((card) => collection.cardIds.includes(card.id))
-      const confidence = aggregateConfidence(cardsInCollection)
-      return { collection, confidence }
-    })
-  })
-
-  protected allCardsWithConfidence = computed(() => {
-    const confidence = aggregateConfidence(this.ls.learnables())
-    const collectionLessCount = this.ls
-      .learnables()
-      .filter(
-        (card) => !this.ls.collections().some((collection) => collection.cardIds.includes(card.id))
-      ).length
-
-    return {
-      confidence,
-      collectionLessCount
-    }
-  })
-}
+export class CardsPage {}
--- projects/frontend/src/app/components/shared/banks-and-collections/cards-quick-selector/cards-quick-selector.ts
+++ projects/frontend/src/app/components/shared/banks-and-collections/cards-quick-selector/cards-quick-selector.ts
@@ -1,22 +1,14 @@
 import { Component, computed, inject } from '@angular/core'
-import { Router, RouterLink } from '@angular/router'
+import { RouterLink } from '@angular/router'
 import { Collection } from '@shared/types'
 import { LearnablesStore } from '../../../../store/learnables-store'
 import { aggregateConfidence, ConfidenceAggregate } from '../../../../utils/genaral-utils'
 import { ConfidenceStats } from '../../confidence/confidence-stats/confidence-stats'
 import { UserCollection } from '../user-collection/user-collection'
 
-type AllCardsSummary = {
-  type: 'all'
-  allCardsCount: number
-  collectionCount: number
-  averageConfidence: ConfidenceAggregate
-  collectionLess: number
-}
-
-type CollectionSummary = Collection & {
-  type: 'collection'
-  averageConfidence: ConfidenceAggregate
+type CollectionWithConfidence = {
+  collection: Collection
+  confidence: ConfidenceAggregate
 }
 
 @Component({
@@ -28,40 +20,28 @@ type CollectionSummary = Collection & {
 export class CardsQuickSelector {
   private readonly ls = inject(LearnablesStore)
 
-  protected readonly collections = this.ls.collections
-  private readonly router = inject(Router)
-
-  protected readonly summaries = computed<(CollectionSummary | AllCardsSummary)[]>(() => {
+  protected collectionsWithConfidence = computed<CollectionWithConfidence[]>(() => {
+    const collections = this.ls.collections()
     const allCards = this.ls.learnables()
-    const collections = this.collections()
 
-    const collectionSummaries: CollectionSummary[] = collections.map((coll) => ({
-      ...coll,
-      type: 'collection',
-      averageConfidence: aggregateConfidence(
-        allCards.filter((card) => coll.cardIds.includes(card.id))
-      )
-    }))
-
-    const collectionIds = collections.flatMap((c) => c.cardIds)
-    const collectionLessCards = allCards.filter((c) => !collectionIds.includes(c.id))
-
-    const allCardsSummary: AllCardsSummary = {
-      type: 'all',
-      allCardsCount: allCards.length,
-      collectionCount: collections.length,
-      averageConfidence: aggregateConfidence(allCards),
-      collectionLess: collectionLessCards.length
-    }
-
-    return [...collectionSummaries, allCardsSummary]
+    return collections.map((collection) => {
+      const cardsInCollection = allCards.filter((card) => collection.cardIds.includes(card.id))
+      const confidence = aggregateConfidence(cardsInCollection)
+      return { collection, confidence }
+    })
   })
 
-  openLink(collectionID?: string) {
-    if (collectionID) {
-      this.router.navigate(['/cards'], { state: { collectionID } })
-    } else {
-      this.router.navigate(['/cards'])
+  protected allCardsWithConfidence = computed(() => {
+    const confidence = aggregateConfidence(this.ls.learnables())
+    const collectionLessCount = this.ls
+      .learnables()
+      .filter(
+        (card) => !this.ls.collections().some((collection) => collection.cardIds.includes(card.id))
+      ).length
+
+    return {
+      confidence,
+      collectionLessCount
     }
-  }
+  })
 }
