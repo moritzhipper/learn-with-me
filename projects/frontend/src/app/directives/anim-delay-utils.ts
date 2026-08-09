96c77b2a5bfc4b5fed2a9fe47e7693aacfedbcbe
mo <moritz.hipper@outlook.de>
2026-08-09 23:31:51 +0200

    WP

--- projects/frontend/src/app/directives/anim-delay-utils.ts
+++ projects/frontend/src/app/directives/anim-delay-utils.ts
@@ -1,16 +1,14 @@
-export type AnimConfig = { i: number; duration?: number } & ({ size: number } | { list: unknown[] })
-
-const DURATION_DEFAULT = 0.2
-const INTERVAL_DELAY_MAX = 0.03
+export type AnimConfig = {
+  i: number
+  size: number
+  duration: number
+}
 
 /**
  * Ensures that the intervaall delay between items of small lists does not become to long, making the animation feel
  */
-export const mapToAnimDelay = (conf: AnimConfig): number => {
-  const size = 'size' in conf ? conf.size : conf.list.length
-  const i = conf.i
-  const duration = conf.duration ?? DURATION_DEFAULT
-  const intervalDelay = Math.min(duration / size, INTERVAL_DELAY_MAX)
+export const mapToAnimDelay = ({ i, size, duration }: AnimConfig): number => {
+  const intervalDelay = duration / size
 
   return i * intervalDelay
 }
--- projects/frontend/src/app/directives/anim-delay-wrapper.ts
+++ projects/frontend/src/app/directives/anim-delay-wrapper.ts
@@ -1,5 +1,4 @@
 import { afterRenderEffect, contentChildren, Directive, ElementRef, input } from '@angular/core'
-import { mapToAnimDelay } from './anim-delay-utils'
 
 @Directive({
   selector: '[lizAnimDelayWrapper]'
@@ -14,24 +13,28 @@ export class AnimDelayWrapper {
    * Applies class to every child with #animItem template ref
    */
   readonly applyClass = input<string>()
-
   readonly duration = input<number>(0.2)
 
   constructor() {
     afterRenderEffect(() => {
       const items = this.items().map((item) => item.nativeElement)
       const duration = this.duration()
+      const size = items.length
 
       items.forEach((item, i) => {
-        const delay = mapToAnimDelay({ i, size: items.length, duration })
-        const applyClass = this.applyClass()
+        const delay = this.mapToDelay(i, size, duration)
+        item.style.setProperty('animation-delay', `${delay}s`)
 
+        const applyClass = this.applyClass()
         if (applyClass) {
           item.classList.add(applyClass)
         }
-
-        item.style.setProperty('animation-delay', `${delay}s`)
       })
     })
+  }
+
+  private mapToDelay(i: number, size: number, duration: number) {
+    const intervalDelay = duration / size
+    return i * intervalDelay
   }
 }
