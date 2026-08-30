ff01714f369baf8e0b6738d60b685ca038fa492f
mo <moritz.hipper@outlook.de>
2026-08-30 13:19:59 +0200

    WP

--- projects/frontend/src/app/components/shared/forms/base-form/base-form.ts
+++ projects/frontend/src/app/components/shared/forms/base-form/base-form.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core'
+import { Component, input, output } from '@angular/core'
 import { AnimDelayWrapper } from '../../../../directives/anim-delay-wrapper'
 
 @Component({
@@ -11,4 +11,6 @@ export class BaseForm {
   readonly header = input.required<string>()
   readonly confirmLabel = input<string>('Confirm')
   readonly cancelLabel = input<string>('Cancel')
+
+  readonly cancel = output<void>()
 }
