fd92303ec54e2e8d3dafb4bb427c79e9bddfba2a
mo <moritz.hipper@outlook.de>
2026-09-12 12:21:13 +0200

    WP

--- projects/frontend/src/app/components/pages/practice-page-comp/practice/practice.ts
+++ projects/frontend/src/app/components/pages/practice-page-comp/practice/practice.ts
@@ -2,10 +2,11 @@ import { Component, computed, inject } from '@angular/core'
 import { LearnablesStore } from '../../../../store/learnables-store'
 import { SwiperNav } from './swiper-nav/swiper-nav'
 import { SwiperSummary } from './swiper-summary/swiper-summary'
+import { Swiper } from './swiper/swiper'
 
 @Component({
   selector: 'liz-practice',
-  imports: [SwiperNav, SwiperSummary],
+  imports: [SwiperNav, SwiperSummary, Swiper],
   templateUrl: './practice.html',
   styleUrl: './practice.scss'
 })
